2c1222df1787d2fcc5e89505e03a7ccf5c1b99b5
Tomas Gutierrez <tomasgutierrez9912@gmail.com>
2022-10-07 15:41:07 -0300

    Modificacion, trabajando con aplicacion y carga de bootstrap

--- app/urls.py
+++ app/urls.py
@@ -14,17 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from . import views
+from django.urls import path,include
 
 urlpatterns = [
+    path('', include('home.urls')),
     path('admin/', admin.site.urls),
-    path('hola/', views.hola),
-    path('fecha/', views.fecha),
-    path('fecha-nacimiento/<int:edad>', views.calcular_fecha),
-    path('mi-template/', views.mi_template),
-    path('tu-template/<str:nombre>', views.tu_template),
-    path('prueba-template/', views.prueba_template),
-    path('ver-personas/', views.ver_personas),
-    path('crear-persona/<str:nombre>/<str:apellido>/', views.crear_persona),
-]
+    ]
